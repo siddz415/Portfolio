4be10c0b954b8ab186555223a18728552ae2e3dc
SiddD <108153549+siddz415@users.noreply.github.com>
2026-05-22 17:18:00 -0700

    Fix name error and ignore docs cache

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <h1 className="h1 mb-6">
               Hello! I&apos;m <br />{" "}
-              <span className="text-blue-600">Siddhath Dravid</span>
+              <span className="text-blue-600">Siddharth Dravid</span>
             </h1>
             <span className="text-xl">Software Developer</span>
             <p className="max-w-[500px] mb-9 text-white/80">
